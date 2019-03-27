db5c9ae6b80958f582c76a76677f84c8d355f71c
admin <admin@DESKTOP-4DQEADR>
2019-03-27 19:37:21 +0530

    First Commit

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,30 @@
+﻿using EmployeeData.Model;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EmployeeData.DataAccess;
+
+namespace EmployeeData.Controllers
+{
+    public class EmployeeController : ControllerBase
+    {
+         public EmployeeDataAccess DataAccess { get; set; }
+        [HttpGet]
+        public IEnumerable<Employee> GetData()
+        {
+            EmployeeDataAccess employeeDataAccess = new EmployeeDataAccess();
+            return employeeDataAccess.GetAllEmployees();
+        }
+
+        [HttpPost]
+        [Route("create")]
+        public IEnumerable<Employee> AddData(Employee employee)
+        {
+            EmployeeDataAccess dataAccess = new EmployeeDataAccess();
+             dataAccess.AddEmployee(employee);
+            return dataAccess.GetAllEmployees();
+        }
+    }
+}
---
+++ DataAccess/EmployeeDataAccess.cs
@@ -0,0 +1,53 @@
+﻿using EmployeeData.Model;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeData.DataAccess
+{
+    public class EmployeeDataAccess
+    {
+        string connectionString = "(localDB)/local";
+        public IEnumerable<Employee> GetAllEmployees()
+        {
+            List<Employee> employees = new List<Employee>();
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand sqlCommand = new SqlCommand("spGetAllEmployees",connection);
+                sqlCommand.CommandType = System.Data.CommandType.StoredProcedure;
+                connection.Open();
+                SqlDataReader sqlDataReader = sqlCommand.ExecuteReader();
+                while(sqlDataReader.Read())
+                {
+                    Employee employee = new Employee();
+                    employee.Id = Convert.ToInt32(sqlDataReader["EmployeeId"]);
+                    employee.Name = sqlDataReader["Name"].ToString();
+                    employee.City = sqlDataReader["City"].ToString();
+                    employee.Department = sqlDataReader["Department"].ToString();
+                    employee.Gender = sqlDataReader["Gender"].ToString();
+                    employees.Add(employee);
+                }
+                connection.Close();
+            }
+            return employees;
+        }
+
+        public void AddEmployee(Employee employee)
+        {
+            using(SqlConnection connection=new SqlConnection(connectionString))
+            {
+                SqlCommand sqlCommand = new SqlCommand("spAddEmployee", connection);
+                sqlCommand.CommandType = System.Data.CommandType.StoredProcedure;
+                sqlCommand.Parameters.AddWithValue("@name",employee.Name);
+                sqlCommand.Parameters.AddWithValue("@City", employee.City);
+                sqlCommand.Parameters.AddWithValue("@Department", employee.Department);
+                sqlCommand.Parameters.AddWithValue("@Gender", employee.Gender);
+                connection.Open();
+                sqlCommand.ExecuteNonQuery();
+                connection.Close();
+            }
+        }
+    }
+}
